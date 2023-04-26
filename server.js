a932c8c604ec3c3fd9171b0f4a596e1d6f78e987
Pirmsho <d.pirmisashvili@gmail.com>
2023-04-26 11:58:47 +0400

    set up db

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((con) => {
+        console.log(con.connection);
+    });
+
 const app = require('./app');
 
 const port = process.env.PORT || 4000;
